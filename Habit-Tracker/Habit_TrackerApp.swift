edadba4bf62157a1afb1da53ac91ef3294e79cc9
Anurag Borisa <anuragborisa@gmail.com>
2025-01-19 10:34:30 +0530

    Initial Commit

---
+++ Habit-Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit-Tracker
+//
+//  Created by Anurag on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
